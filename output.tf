d13f0a6d8a1b9fd96a3825ccdfb0775f0d46fc6a
akshata2890 <53058275+akshata2890@users.noreply.github.com>
2019-09-03 01:47:26 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -4,10 +4,6 @@ output "target_azure_subscription" {
   value = "${data.azurerm_subscription.currentsub.display_name}"
 }
 
-#output "instrumentation_key" {
-  #value = "${azurerm_application_insights.ai.instrumentation_key}"
-#}
-
 output "appservice_dns_name" {
   value = "${azurerm_app_service.appsvc.default_site_hostname}"
 }
